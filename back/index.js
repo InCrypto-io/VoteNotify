bde10d87d0f99ad6f5382981c89cb13242cf9e27
Vadim <vadim.pysarenko97@gmail.com>
2018-11-07 15:14:52 +0200

    add cors

--- back/index.js
+++ back/index.js
@@ -1,5 +1,6 @@
 const EosApp = require ('./EosApp');
 var express = require('express');
+var cors = require('cors');
 
 var eosapp = new EosApp.EosApp();
 setTimeout(() => { eosapp.updateVoters() }, 500);
@@ -7,6 +8,7 @@ setTimeout(() => { eosapp.updateVoters() }, 500);
 var app = express();
 app.set('eosapp', eosapp);
 app.use(express.json());
+app.use(cors());
 app.use(require('./routes'));
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
